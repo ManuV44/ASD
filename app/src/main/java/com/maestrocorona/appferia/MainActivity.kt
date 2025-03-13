73f48ac4320b4e603346bdb96b1182bcc59bbc19
ManuV44 <09manuel0408@gmail.com>
2025-03-12 21:49:37 -0600

    Entrega

--- app/src/main/java/com/maestrocorona/appferia/MainActivity.kt
+++ app/src/main/java/com/maestrocorona/appferia/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.maestrocorona.appferia
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import android.content.Intent
+import androidx.compose.ui.unit.sp
+import com.maestrocorona.appferia.ui.theme.Purple40
+import com.maestrocorona.appferia.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +63,8 @@ fun MainScreen(onNavigateToSecondActivity: () -> Unit) {
             BusinessItem("Negocios de la Nave 1")
             BusinessItem("Negocios de la Nave 2")
             BusinessItem("Negocios de la Nave 3")
-            
+            BusinessItem("Atracciones y Conciertos")
+
             // Botón para navegar a la segunda actividad
             Button(
                 onClick = onNavigateToSecondActivity,
@@ -67,8 +86,9 @@ fun BusinessItem(text: String) {
     ) {
         Row(
             modifier = Modifier
+                .background(Purple80)
                 .fillMaxSize()
-                .padding(8.dp),
+                .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             // Imagen del restaurante
@@ -82,8 +102,27 @@ fun BusinessItem(text: String) {
             // Texto del negocio
             Text(
                 text = text,
-                modifier = Modifier.padding(8.dp)
+                style =
+                    TextStyle(
+                        color = Purple40,
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.SansSerif,
+                        //fontWeight = FontWeight.W800,
+                        //fontStyle = FontStyle.Italic,
+                        //letterSpacing = 0.5.em,
+                        //background = Color.LightGray,
+                        //textDecoration = TextDecoration.Underline
+                    )
+                //modifier = Modifier.padding(8.dp)
+
             )
         }
     }
+}
+
+
+@Preview(showBackground = true, name = "Previsualización de MainScreen")
+@Composable
+fun PreviewMainScreen() {
+    MainScreen(onNavigateToSecondActivity = {})
 }
